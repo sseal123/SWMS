8fe613b3b00b039878160e0117bd3f5970481f0c
Sourav Seal <sourav.seal18@gmail.com>
2021-10-08 15:11:13 +0530

    Add project files.

---
+++ SWMS/Models/EmployerModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWMS.Models
+{
+    public class EmployerModel
+    {
+        public string Employer { get; set; }
+        public string BusinessName { get; set; }
+        public string BusinessContact { get; set; }
+        public string BusinessAddress { get; set; }
+
+        public string ABN { get; set; }
+        public string Email {get;set;}
+
+        public string PhonoNo { get; set; }
+        public string MobilePhNo { get; set; }
+
+        public string Website { get; set; }
+
+    }
+}
